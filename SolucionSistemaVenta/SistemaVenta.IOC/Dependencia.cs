01accd7a592a2a8172e18b3362eafdf0ab21590c
JoelDelAbra1 <alejandro.delabrapnd@uanl.edu.mx>
2023-09-09 22:39:41 -0600

    Se agrega video #5

--- SolucionSistemaVenta/SistemaVenta.IOC/Dependencia.cs
+++ SolucionSistemaVenta/SistemaVenta.IOC/Dependencia.cs
@@ -9,8 +9,9 @@ using SistemaVenta.DAL.DBContext;
 using Microsoft.EntityFrameworkCore;
 using SistemaVenta.DAL.Implementacion;
 using SistemaVenta.DAL.Interfaces;
-//using SistemaVenta.BLL.Implementacion;
-//using SistemaVenta.BLL.Interfaces;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using SistemaVenta.BLL.Implementacion;
+using SistemaVenta.BLL.Interfaces;
 
 namespace SistemaVenta.IOC
 {
@@ -26,6 +27,8 @@ namespace SistemaVenta.IOC
             services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
             services.AddScoped<IVentaRepository, VentaRepository>();
+
+            services.AddScoped<ICorreoService, CorreoService>();
         }
     }
 }
