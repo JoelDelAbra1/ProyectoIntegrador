4ed6198396b7a5d01c2d71c351f0b5bb343c932a
JoelDelAbra1 <alejandro.delabrapnd@uanl.edu.mx>
2023-09-16 15:49:08 -0600

    Se agregan los archivos del video #7

---
+++ SolucionSistemaVenta/SistemaVenta.AplicacionWeb/Utilidades/Response/GenericResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace SistemaVenta.AplicacionWeb.Utilidades.Response
+{
+    public class GenericResponse<TObject>
+    {
+        public bool Estaado { get; set; }
+
+        public string? Mensaje { get; set; } // ? perimite nulos
+
+        public TObject? Object { get; set; }
+
+        public List<TObject>? ListaObjecto{ get; set; }
+    }
+}
