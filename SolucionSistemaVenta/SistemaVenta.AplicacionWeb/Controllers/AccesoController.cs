6e9dc3a6ed91f64e5aa36328ae924c55392383a5
Cesar R <cegar@DESKTOP-HTD0692>
2023-09-24 22:01:34 -0600

    Video Completo 28 (hay que revisar las vistas de los videos 22,23 )

--- SolucionSistemaVenta/SistemaVenta.AplicacionWeb/Controllers/AccesoController.cs
+++ SolucionSistemaVenta/SistemaVenta.AplicacionWeb/Controllers/AccesoController.cs
@@ -33,6 +33,16 @@ namespace SistemaVenta.AplicacionWeb.Controllers
             return View();
         }
 
+
+        public IActionResult RestablecerClave()
+        {
+
+            
+
+            return View();
+        }
+
+
         [HttpPost]
         public async Task<IActionResult> Login(VMUsuarioLogin modelo)
         {
@@ -70,5 +80,39 @@ namespace SistemaVenta.AplicacionWeb.Controllers
 
             return RedirectToAction("Index", "Home");
         }
+
+
+        [HttpPost]
+        public async Task<IActionResult> RestablecerClave(VMUsuarioLogin modelo)
+        {
+
+            try
+            {
+                string urlPlantillaCorreo = $"{this.Request.Scheme}://{this.Request.Host}/Plantilla/RestablecerClave?clave=[clave]";
+
+                bool resultado = await _usuarioServico.RestablecerClave(modelo.Correo, urlPlantillaCorreo);
+
+                if (resultado)
+                {
+                    ViewData["Mensaje"] = "Listo, su contraseña fue restablecida. Revise su correo";
+                    ViewData["MensajeError"] = null;
+                }
+                else
+                {
+                    ViewData["MensajeError"] = "Tenemos un error. Porfavor intentelo de nuevo mas tarde";
+                    ViewData["Mensaje"] = null;
+                }
+
+            } catch (Exception ex)
+            {
+                ViewData["MensajeError"] = ex.Message;
+                ViewData["Mensaje"] = null;
+            }
+
+
+
+            return View();
+        }
+
     }
 }
